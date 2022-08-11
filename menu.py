e5bd9e6c4097b81da2eac7e036d65fd91ef1d5b1
jecar07 <jecardona11@gmail.com>
2022-08-11 18:51:09 -0500

    nuevo menu

---
+++ menu.py
@@ -0,0 +1,21 @@
+print("""
+    1) Herramientas       3) Temperatura
+    2) Calculadora        4) Mas...
+    """)
+
+# Leemos lo que ingresa el usuario
+eligio=input("-Selecciona algo :")
+
+# Según lo que ingresó, código diferente
+if eligio=="1":
+    print("suma")
+elif eligio=="2":
+    x = 3
+    y = 5
+    print("x * y = ", x * y)
+elif eligio=="3":
+    print("Creo que hace frío")
+elif eligio=="4":
+    print("otra opción")
+else:
+    print("Opción no válida")
