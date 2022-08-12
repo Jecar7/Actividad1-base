ebd2b89e6849aed4ddeb99ee3330ef5906f2598f
jecar07 <jecardona11@gmail.com>
2022-08-11 22:20:11 -0500

    cambio 4

--- menu.py
+++ menu.py
@@ -17,8 +17,20 @@ if eligio=="1":
         print(num3)
      elif num1>num3 and num3>num2:
         print(num1)
+        print(num3)
+        print(num2)
+     elif num2>num1 and num1>num3:
+        print(num2)
+        print(num1)
+        print(num3)    
+     elif num2>num3 and num3>num1:
         print(num2)
         print(num3)
+        print(num1)
+     elif num2>num3 and num3>num1:
+        print(num2)
+        print(num3)
+        print(num1)   
     except:
       print("Escribir solo numeros enteros")
 if eligio=="2":
